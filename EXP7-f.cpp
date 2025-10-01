11f9126ef57d2010c5766ba56599575d7f9c9182
imdhairyathakkar-spec <imdhairyathakkar@gmail.com>
2025-10-01 17:31:05 +0530

    Create EXP7-f.cpp

---
+++ EXP7-f.cpp
@@ -0,0 +1,26 @@
+//Dhairya Thakkar
+//24070123037
+//A2
+#include <iostream>
+using namespace std;
+int main(){
+    string a,b;
+    cout << "enter the first string"<<endl;
+    cin>>a;
+    cout << "enter the second string"<<endl;
+    cin >>b;
+    string c ;
+    c=a+" "+b;
+    cout << "the concatenated string is "<<c<<endl;
+
+    return 0;}
+
+
+/*OUTPUT:
+enter the first string
+harsh
+enter the second string
+kumar
+the concatenated string is harsh kumar
+
+*/
