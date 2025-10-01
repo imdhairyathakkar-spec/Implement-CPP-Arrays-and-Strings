88780c20eaae592bce111fb5566d37fb7cb948b0
imdhairyathakkar-spec <imdhairyathakkar@gmail.com>
2025-10-01 17:30:16 +0530

    Create EXP7-e.cpp

---
+++ EXP7-e.cpp
@@ -0,0 +1,24 @@
+//Dhairya Thakkar
+//24070123037
+//A2
+#include <iostream>
+using namespace std;
+int main(){
+    char a[4]="Hii" ; //taking a array of characters
+    char b[]={'h','i','i','\0'};
+    char c[4]={'H','i','i','\0'};
+    string d ="hii"; 
+    cout << "Different ways to declare a string in C++ : "<<endl;
+    cout <<a<<endl;
+    cout <<b<<endl;
+    cout << c<<endl;
+    cout << d<<endl;
+return 0;
+}
+
+/* 
+Output:  Different ways to declare a string in C++ : 
+Hii
+hii
+Hii
+hii */
